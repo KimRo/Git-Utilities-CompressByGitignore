f8f926ee1d457d5017fc1d4fc2b23dd1af9c041f
Kim Robbens <kim.robbens@gmail.com>
2023-07-31 12:21:21 +0200

    Add files via upload

--- git-compress.bat
+++ git-compress.bat
@@ -1,4 +1,4 @@
 cd %1
 for %%I in (.) do set CurrDirName=%%~nxI
-git ls-files > ..\files.txt  && "C:\Program Files\7-Zip\7z.exe" a ..\"%CurrDirName%".7z @..\files.txt && del ..\files.txt
+"C:\Program Files\git\bin\git.exe" ls-files > ..\files.txt  && "C:\Program Files\7-Zip\7z.exe" a ..\"%CurrDirName%".7z @..\files.txt && del ..\files.txt
 cd ..
